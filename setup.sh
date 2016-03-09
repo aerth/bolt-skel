599af6f4977b945b58c6e27bc3e2564298a40fe9
aerth <aerth@users.noreply.github.com>
2016-03-08 16:03:08 -0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,22 +1,28 @@
 #!/bin/sh
 
 if [ -z "$1" ];then
+
+echo "#########################################"
+echo ""
+echo "Usage:"
+
 echo docker run -it -v /cattle:/opt/cattle alpine:latest sh
-
+echo ""
 echo apk --update add git
-
+echo ""
 echo git clone https://github.com/aerth/bolt-skel /cattle/bolt-skel
-
+echo ""
 echo exit
-
+echo ""
 echo $0 boltskel 8080
 
-
 echo "$0 [cattle-name] [port]"
+echo ""
+echo "#########################################"
 exit 1
 fi
 if [ -z "$2" ];then
-echo "$0 [cattle-name] [port]"
+echo "Usage: $0 [cattle-name] [port]"
 exit 1
 fi
 echo Launching cattle $1 into $2 space!
@@ -34,5 +40,4 @@ docker run -it -d -p $2:80 \
 -v /cattle/$1/public/files:/var/www/public/files \
 --name $1 aerth/boltcms:latest
 
-echo Visit http://127.0.0.1:$2 (or http://$1)
-
+echo "Visit http://127.0.0.1:$2 (or http://$1)"
